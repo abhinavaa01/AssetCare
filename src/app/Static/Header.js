5b514af18f132e0bf2d5e171cca20d293168ac6a
abhinavaa01 <agarwalabhinav91@gmail.com>
2024-11-12 17:02:06 +0530

    login code

--- src/app/Static/Header.js
+++ src/app/Static/Header.js
@@ -5,6 +5,7 @@ import "./static.css"
 import { auth } from '@/Services/firebase';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useState } from 'react';
+import { redirect } from 'next/navigation';
 
 export default function Header() {
     const [currentUser, setCurrentUser] = useState();
@@ -33,7 +34,7 @@ export default function Header() {
                     <img src={logo.src} className="h-100 w-100 my-auto" alt="Todo"></img>
                     <h2 className='my-auto ms-2 text-dark'>AssetCare</h2>
                 </a>
-                {true ?
+                {currentUser ?
                     <div className='dropdown w-auto ms-auto'>
                         {/* <button onClick={logout} className='btn btn-info ms-auto me-3 w-auto h-50 my-auto'>Logout</button> */}
                         <button className="header-user-btn fs-2 btn btn-default p-0 m-2 dropdown-toggle my-auto" type="button" id="menu1" data-bs-toggle="dropdown" aria-expanded="false">
@@ -49,8 +50,7 @@ export default function Header() {
                         </ul>
                     </div> :
                     <>
-                        <a href="/login" className='btn btn-info ms-auto w-auto h-50 my-auto'>Login</a>
-                        <a href="/signup" className='btn btn-info ms-3 me-3 w-auto h-50 my-auto'>Signup</a>
+                        <button onClick={()=>redirect("/login")} className='btn btn-info ms-auto w-auto h-50 my-auto'>Login</button>
                     </>}
             </div>
         </div>
