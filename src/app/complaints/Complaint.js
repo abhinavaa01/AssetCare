63be359246584e52cad30585035388b5b796cade
abhinavaa01 <agarwalabhinav91@gmail.com>
2024-12-03 21:59:57 +0530

    Complaint card

--- src/app/complaints/Complaint.js
+++ src/app/complaints/Complaint.js
@@ -1,15 +1,16 @@
 export default function Complaint(props) {
     const { data, key, index } = props;
-    // console.log( data, index+1);
+    // console.log( data );
+    const complaintDate = new Date(data.createDate.toMillis());
     return (
         <div className="col-4 col-md-3 shadow-sm card p-3 mx-2">
-            <div className="">Complaint Number: {index+1}</div>
+            <div className="fw-bold">Complaint Number: {index+1}</div>
             <div className="">Complaint Id: {data.id}</div>
             <div className="">Name: {data.userName}</div>
             <div className="">Phone: {data.userPhone}</div>
             <div className="">Email: {data.userEmail}</div>
-            {/* <div className="">Registration date: {data.createDate.toDateString()}</div>
-            <div className="">Registration Time: {data.createDate.toTimeString()}</div> */}
+            <div className="">Registration date: {complaintDate.toDateString()}</div>
+            <div className="">Registration Time: {complaintDate.toTimeString()}</div>
             <div className="">Status: {data.status}</div>
         </div>
     )
