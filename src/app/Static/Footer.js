a7eb6de2d54fa0d9f21276f8e698f4c0869b59c3
abhinavaa01 <agarwalabhinav91@gmail.com>
2024-12-04 15:05:52 +0530

    Footer Updated

--- src/app/Static/Footer.js
+++ src/app/Static/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="mt-2 row border fixed-bottom bg-light">
-      <p className="my-auto col-4 ms-3 my-2">© Abhinav Agrawal</p>
+      <p className="my-auto col-4 ms-3 my-2">© Group 13 (Abhinav Agrawal, Vishal Sharma, Prashant Yadav)</p>
     </div>
   );
 }
